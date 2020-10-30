258d783d410b3cc853e1bc4fdd42ed925a6eedf8
hamdibenslama <62665471+hamdibenslama@users.noreply.github.com>
2020-10-31 00:53:50 +0100

    Add files via upload

---
+++ Tp1_Android/app/src/main/java/com/example/tp1_android/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tp1_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        Log.i("MainActivity", "Hello");
+    }
+}
+
